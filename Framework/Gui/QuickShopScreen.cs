68a254f14129ddd974dc2ff9d0d03112a161186c
Enaium <32991121+Enaium@users.noreply.github.com>
2021-01-13 21:38:50 +0800

    Add mailbox,changeAppearance

--- Framework/Gui/QuickShopScreen.cs
+++ Framework/Gui/QuickShopScreen.cs
@@ -5,6 +5,7 @@ using EnaiumToolKit.Framework.Screen;
 using EnaiumToolKit.Framework.Screen.Elements;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Locations;
 using StardewValley.Menus;
@@ -19,7 +20,8 @@ namespace QuickShop.Framework.Gui
     {
         public QuickShopScreen()
         {
-            string buttonTitle = GetTranslation("quickShop.button");
+            var buttonTitle = GetTranslation("quickShop.button");
+            var gameLocation = Game1.game1.instanceGameLocation;
 
             string pierreShopTitle = $"{buttonTitle} {GetButtonTranslation("pierreShop")}";
             AddElement(new Button(pierreShopTitle, pierreShopTitle)
@@ -55,8 +57,8 @@ namespace QuickShop.Framework.Gui
                 }
             });
 
-            string carpenterTitle = $"{buttonTitle} {GetButtonTranslation("carpenter")}";
-            AddElement(new Button(carpenterTitle, carpenterTitle)
+            string carpenterBuildingTitle = $"{buttonTitle} {GetButtonTranslation("carpenterBuilding")}";
+            AddElement(new Button(carpenterBuildingTitle, carpenterBuildingTitle)
             {
                 OnLeftClicked = () => { Game1.activeClickableMenu = new CarpenterMenu(); }
             });
@@ -168,6 +170,12 @@ namespace QuickShop.Framework.Gui
             AddElement(new Button(geodeTitle, geodeTitle)
             {
                 OnLeftClicked = () => { Game1.activeClickableMenu = new GeodeMenu(); }
+            });
+
+            string mailboxTitle = $"{buttonTitle} {GetButtonTranslation("mailbox")}";
+            AddElement(new Button(mailboxTitle, mailboxTitle)
+            {
+                OnLeftClicked = () => { gameLocation.mailbox(); }
             });
 
             string calendarTitle = $"{buttonTitle} {GetButtonTranslation("calendar")}";
@@ -292,11 +300,13 @@ namespace QuickShop.Framework.Gui
             {
                 OnLeftClicked = () =>
                 {
-                    Game1.activeClickableMenu = new ShopMenu(sandyShopStock(), who: "Sandy",
-                        on_purchase: onSandyShopPurchase);
+                    Game1.activeClickableMenu =
+                        new ShopMenu(
+                            GetMethod(gameLocation, "sandyShopStock")
+                                .Invoke<Dictionary<ISalable, int[]>>(), who: "Sandy",
+                            on_purchase: OnSandyShopPurchase);
                 }
             });
-
 
             string desertShopTitle = $"{buttonTitle} {GetButtonTranslation("desertShop")}";
             AddElement(new Button(desertShopTitle, desertShopTitle)
@@ -355,10 +365,21 @@ namespace QuickShop.Framework.Gui
                 }
             });
 
-            string wizardTitle = $"{buttonTitle} {GetButtonTranslation("wizard")}";
-            AddElement(new Button(wizardTitle, wizardTitle)
+            string wizardBuildingTitle = $"{buttonTitle} {GetButtonTranslation("wizardBuilding")}";
+            AddElement(new Button(wizardBuildingTitle, wizardBuildingTitle)
             {
                 OnLeftClicked = () => { Game1.activeClickableMenu = new CarpenterMenu(true); }
+            });
+
+            string changeAppearanceTitle = $"{buttonTitle} {GetButtonTranslation("changeAppearance")}";
+            AddElement(new Button(changeAppearanceTitle, changeAppearanceTitle)
+            {
+                OnLeftClicked = () =>
+                {
+                    gameLocation.createQuestionDialogue(
+                        Game1.content.LoadString("Strings\\Locations:WizardTower_WizardShrine").Replace('\n', '^'),
+                        gameLocation.createYesNoResponses(), "WizardShrine");
+                }
             });
 
             if (!Game1.player.mailReceived.Contains("JojaMember"))
@@ -366,7 +387,7 @@ namespace QuickShop.Framework.Gui
                 string bundlesTitle = $"{buttonTitle} {GetButtonTranslation("bundles")}";
                 AddElement(new Button(bundlesTitle, bundlesTitle)
                 {
-                    OnLeftClicked = () => { Game1.activeClickableMenu = new JunimoNoteMenu(true, 1, true); }
+                    OnLeftClicked = () => { Game1.activeClickableMenu = new JunimoNoteMenu(true); }
                 });
             }
 
@@ -597,124 +618,6 @@ namespace QuickShop.Framework.Gui
             }
         }
 
-        private Dictionary<ISalable, int[]> sandyShopStock()
-        {
-            Dictionary<ISalable, int[]> dictionary = new Dictionary<ISalable, int[]>();
-            Utility.AddStock(dictionary, new Object(802, int.MaxValue),
-                (int) (75.0 * Game1.MasterPlayer.difficultyModifier));
-            Utility.AddStock(dictionary, new Object(478, int.MaxValue));
-            Utility.AddStock(dictionary, new Object(486, int.MaxValue));
-            Utility.AddStock(dictionary, new Object(494, int.MaxValue));
-            Dictionary<ISalable, int[]> stock = dictionary;
-            Object @object = new Object(Vector2.Zero, 196) {Stack = int.MaxValue};
-            Utility.AddStock(stock, @object);
-            switch (Game1.dayOfMonth % 7)
-            {
-                case 0:
-                    Utility.AddStock(dictionary, new Object(233, int.MaxValue));
-                    break;
-                case 1:
-                    Utility.AddStock(dictionary, new Object(88, 1), 200, 10);
-                    break;
-                case 2:
-                    Utility.AddStock(dictionary, new Object(90, int.MaxValue));
-                    break;
-                case 3:
-                    Utility.AddStock(dictionary, new Object(749, 1), 500, 3);
-                    break;
-                case 4:
-                    Utility.AddStock(dictionary, new Object(466, int.MaxValue));
-                    break;
-                case 5:
-                    Utility.AddStock(dictionary, new Object(340, int.MaxValue));
-                    break;
-                case 6:
-                    Utility.AddStock(dictionary, new Object(371, int.MaxValue), 100);
-                    break;
-            }
-
-            Random random = new Random((int) Game1.stats.DaysPlayed + (int) Game1.uniqueIDForThisGame / 2);
-            Clothing clothing = new Clothing(1000 + random.Next(sbyte.MaxValue));
-            dictionary.Add(clothing, new[]
-            {
-                1000,
-                int.MaxValue
-            });
-            dictionary.Add(new Furniture(2655, Vector2.Zero), new[]
-            {
-                700,
-                int.MaxValue
-            });
-            switch (Game1.dayOfMonth % 7)
-            {
-                case 0:
-                    dictionary.Add(new Furniture(2720, Vector2.Zero), new[]
-                    {
-                        3000,
-                        int.MaxValue
-                    });
-                    break;
-                case 1:
-                    dictionary.Add(new Furniture(2802, Vector2.Zero), new[]
-                    {
-                        2000,
-                        int.MaxValue
-                    });
-                    break;
-                case 2:
-                    dictionary.Add(new Furniture(2734 + random.Next(4) * 2, Vector2.Zero), new[]
-                    {
-                        500,
-                        int.MaxValue
-                    });
-                    break;
-                case 3:
-                    dictionary.Add(new Furniture(2584, Vector2.Zero), new[]
-                    {
-                        5000,
-                        int.MaxValue
-                    });
-                    break;
-                case 4:
-                    dictionary.Add(new Furniture(2794, Vector2.Zero), new[]
-                    {
-                        2500,
-                        int.MaxValue
-                    });
-                    break;
-                case 5:
-                    dictionary.Add(new Furniture(2784, Vector2.Zero), new[]
-                    {
-                        2500,
-                        int.MaxValue
-                    });
-                    break;
-                case 6:
-                    dictionary.Add(new Furniture(2748, Vector2.Zero), new[]
-                    {
-                        500,
-                        int.MaxValue
-                    });
-                    dictionary.Add(new Furniture(2812, Vector2.Zero), new[]
-                    {
-                        500,
-                        int.MaxValue
-                    });
-                    break;
-            }
-
-            Game1.player.team.synchronizedShopStock.UpdateLocalStockWithSyncedQuanitities(
-                SynchronizedShopStock.SynchedShop.Sandy, dictionary);
-            return dictionary;
-        }
-
-        private bool onSandyShopPurchase(ISalable item, Farmer who, int amount)
-        {
-            Game1.player.team.synchronizedShopStock.OnItemPurchased(SynchronizedShopStock.SynchedShop.Sandy, item,
-                amount);
-            return false;
-        }
-
         private string GetButtonTranslation(string key)
         {
             return ModEntry.GetInstance().Helper.Translation.Get("quickShop.button." + key);
@@ -723,6 +626,13 @@ namespace QuickShop.Framework.Gui
         private string GetTranslation(string key)
         {
             return ModEntry.GetInstance().Helper.Translation.Get(key);
+        }
+
+        private bool OnSandyShopPurchase(ISalable item, Farmer who, int amount)
+        {
+            Game1.player.team.synchronizedShopStock.OnItemPurchased(SynchronizedShopStock.SynchedShop.Sandy, item,
+                amount);
+            return false;
         }
 
         private ItemGrabMenu ShippingBin()
@@ -736,6 +646,11 @@ namespace QuickShop.Framework.Gui
             itemGrabMenu.setDestroyItemOnClick(true);
             itemGrabMenu.initializeShippingBin();
             return itemGrabMenu;
+        }
+
+        private IReflectedMethod GetMethod(object obj, string name)
+        {
+            return ModEntry.GetInstance().Helper.Reflection.GetMethod(obj, name);
         }
     }
 }
